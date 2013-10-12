555e6e3ced35fb8aa4313eee9fd68fe7b6a40ab0
PaulaEs <paulaproxy@gmail.com>
2013-10-13 01:35:17 +0200

    New examples.

--- practicing06.py
+++ practicing06.py
@@ -1,3 +1,4 @@
+######################
 # looping over indices
 
 
@@ -40,7 +41,7 @@ def count_adjacent_repeats(s):
     return repeats
 
 
-
+#################
 # parallel lists:
 
 def sum_items(list1, list2):
@@ -82,5 +83,55 @@ def count_matches(s1, s2):
             num_matches = num_matches + 1
 
     return num_matches
+
+##############
+# nested lists
+
+def calculate_average(asn_grades):
+    ''' (list of list of [str, number]) -> float
+
+
+    Return the average of the grades in asn_grades.
+
+    >>> calculate_average([['A1', 80], ['A2', 90]])
+    85.0
+    '''
+    
+    total = 0
+
+    for item in asn_grades:
+        total = total + item[1]
+
+    return total / len(asn_grades)
+
+
+##############
+# nested loops
+
+def averages(grades):
+    ''' (list of list of number) -> list of float
+
+    Return a new list in which each item is the average of the grades
+    in the inner list at the corresponding position of grades.
+    
+    >>> averages([[70, 75, 80], [70, 80, 90, 100], [80, 100]])
+    [75.0, 85.0, 90.0]
+    '''
+
+    averages = []
+
+    for grades_list in grades:
+
+        # calculate the average of grade_list and append it to averages.
+
+        total = 0
+        for mark in grades_list:
+            total = total + mark
+
+        averages.append(total / len(grades_list))
+
+    return averages
+
+
 
 
